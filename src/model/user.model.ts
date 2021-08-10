a52ff7449e248495a0dea3b32d426b332eb0a9cc
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2021-08-10 15:58:32 +0530

    database changes

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,4 +1,6 @@
-import mongoose from 'mongoose';
+import mongoose = require("mongoose");
+import * as bcrypt from 'bcrypt'
+import { IUserInformation } from '../interface/IUser.interface';
 
 
 const UserSchema = new mongoose.Schema({
@@ -17,5 +19,28 @@ const UserSchema = new mongoose.Schema({
         required: true
     }
 })
+UserSchema.pre<IUserInformation>('save', async function(next: (arg0: any) => void){
+    try {
+        
+        const salt = await bcrypt.genSalt(5);
+        const hashPassword = await bcrypt.hash(this.password, salt);
+    
+        console.log('password', hashPassword)
+    
+        this.password = hashPassword;
+    
+    
+    } catch (error) {
+        next(error)
+    }
+    })
+    
+    UserSchema.methods.isValidPassword= async function (password: string | Buffer, hashPassword: string) {
+        try {
+          return await bcrypt.compare(password, hashPassword)
+        } catch (error) {
+          throw error
+        }
+      }
 
-export const User = mongoose.model('users', UserSchema );
+export const User = mongoose.model<IUserInformation & mongoose.Document>('users', UserSchema );
